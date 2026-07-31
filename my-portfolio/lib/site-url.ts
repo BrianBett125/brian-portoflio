3698411eef3f2da3392ab6384e5f43db9c0524c1
BrianBett125 <brianbett756@gmail.com>
2026-08-01 00:30:33 +0300

    fix: enhance getSiteUrl function to prioritize Vercel environment variables

--- my-portfolio/lib/site-url.ts
+++ my-portfolio/lib/site-url.ts
@@ -5,6 +5,14 @@ export function getSiteUrl() {
     return process.env.NEXT_PUBLIC_SITE_URL;
   }
 
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+
+  if (process.env.VERCEL_BRANCH_URL) {
+    return `https://${process.env.VERCEL_BRANCH_URL}`;
+  }
+
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
